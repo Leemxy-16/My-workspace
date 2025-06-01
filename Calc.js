4a2ee0d5eb0007dd2fd78de38e3904a5e02e4d26
Leemxy-16 <herleemahaliyu@gmail.com>
2025-06-01 12:01:48 +0100

    Add files via upload

---
+++ Calc.js
@@ -0,0 +1,24 @@
+let display = document.getElementById('display');
+
+
+function appendCharacter(char) {
+  display.value += char;
+}
+
+
+function clearDisplay() {
+  display.value = '';
+}
+
+function deleteChar() {
+  display.value = display.value.slice(0, -1);
+}
+
+function calculateResult() {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = 'Error';
+  }
+}
+
