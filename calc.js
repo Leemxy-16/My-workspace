4a1f3ee4a582351b7cdc0823b07cdd6997e524bd
Leemxy-16 <herleemahaliyu@gmail.com>
2025-06-03 15:55:30 +0100

    Update calc.js

--- calc.js
+++ calc.js
@@ -10,6 +10,19 @@ function clearDisplay() {
   display.value = '';
 }
 
+function calculateResult() {
+  try {
+    // Only allow numbers and operators
+    if (/^[\d+\-*/.() ]+$/.test(display.value)) {
+      display.value = Function('"use strict";return (' + display.value + ')')();
+    } else {
+      display.value = 'Error';
+    }
+  } catch (error) {
+    display.value = 'Error';
+  }
+}
+
 function deleteChar() {
   display.value = display.value.slice(0, -1);
 }
